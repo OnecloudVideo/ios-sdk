10883a92c52b29d326d5a25f8dfd2c07aa164d27
OnecloudVideo <open.video@onecloud.cn>
2015-07-31 17:42:34 +0800

    Initial Commit

---
+++ VideoSDK/VideoSDK.h
@@ -0,0 +1,19 @@
+//
+//  VideoSDK.h
+//  VideoSDK
+//
+//  Created by kinghai on 7/31/15.
+//  Copyright (c) 2015 onecloud.ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VideoSDK.
+FOUNDATION_EXPORT double VideoSDKVersionNumber;
+
+//! Project version string for VideoSDK.
+FOUNDATION_EXPORT const unsigned char VideoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoSDK/PublicHeader.h>
+
+
